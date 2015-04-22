70534e6a867e18dfdb0fa3664ff253ba38f4a95d
Maga Abdurakhmanov <maga@inn.eu>
2015-04-23 00:24:21 +0200

    - rolled back

--- src/main/scala/eu/inn/binders/annotations/fieldName.scala
+++ src/main/scala/eu/inn/binders/annotations/fieldName.scala
@@ -2,5 +2,4 @@ package eu.inn.binders.annotations
 
 import scala.annotation.StaticAnnotation
 
-trait FieldNameMarker
-case class fieldName(name: String, applyConverter: Boolean = false) extends StaticAnnotation with FieldNameMarker
+case class fieldName(name: String, applyConverter: Boolean = false) extends StaticAnnotation
