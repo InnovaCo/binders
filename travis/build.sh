8e2e32a32ae45d66d08af5f9247a9fb327d8f996
Maga Abdurakhmanov <maqdev@gmail.com>
2016-09-03 17:29:25 +0200

    * testing snapshot publishing

--- travis/build.sh
+++ travis/build.sh
@@ -2,12 +2,14 @@
 set -e
 
 if [[ "$TRAVIS_PULL_REQUEST" == "false" && "$TRAVIS_BRANCH" == "master" ]]; then
-openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-public.enc -out ./inn-oss-public.asc -d
-openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-private.enc -out ./inn-oss-private.asc -d
+  openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-public.enc -out ./inn-oss-public.asc -d
+  openssl aes-256-cbc -k "$key_password" -in ./travis/inn-oss-private.enc -out ./inn-oss-private.asc -d
+
   if grep "version\s*:=.*SNAPSHOT" build.sbt; then
     sbt +test +publishSigned
   else
     #sbt 'set version := version.value + "." + System.getenv("TRAVIS_BUILD_NUMBER")' +test +publishSigned sonatypeReleaseAll
+    echo test1
   fi
 else
   sbt +test
